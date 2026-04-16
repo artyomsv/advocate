40a2ccfe8db872179605550db2800b2fe06b232f
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-04-16 17:48:14 +0200

    feat(app): expose legend-accounts module via barrel

---
+++ packages/app/src/legend-accounts/index.ts
@@ -0,0 +1,6 @@
+export * from './account.repository.js';
+export * from './account.service.js';
+export * from './credential.service.js';
+export * from './errors.js';
+export * from './types.js';
+export * from './validation.js';
